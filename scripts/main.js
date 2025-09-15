1003aae42f0b58224d41408707451060be482d3c
Kavinkumar <mkavinkumarnaidu@gmail.com>
2025-09-15 15:50:48 +0530

    First Commit

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+let directory = document.getElementById("directory");
+const txtSelected = document.getElementById("selected");
+
+const btnAddToSpeedDiall = document.getElementById("addToSpeedDial");
+const btnCallSpeedDiall = document.getElementById("callSpeedDial");
+
+let txtParas;
+let selected;
+
+btnAddToSpeedDiall.addEventListener("click", addToSpeedDial);
+btnCallSpeedDiall.addEventListener("click", callSpeedDial);
+
+getData();
+
+function getData() {
+  fetch("directory.json")
+    .then((res) => res.json())
+    .then((data) => processData(data))
+    .catch((error) => console.log(`Error - ${error}`));
+}
+
+function processData(data) {
+  localStorage.clear();
+  let dirList = "";
+  for (let i = 0; i < data.length; i++) {
+    dirList += `<p class='entry'>${data[i]["name"]} (  
+        ${data[i]["type"]} ) : ${data[i]["number"]}</p>`;
+  }
+
+  directory.innerHTML = dirList;
+
+  // Grabs a collection (array) of all the P tags.
+  txtParas = Array.from(document.getElementsByClassName("entry"));
+  console.log(txtParas);
+  txtParas.forEach((item) => item.addEventListener("click", displaySelected));
+}
+
+function displaySelected() {
+  for (let i = 0; i < txtParas.length; i++) {
+    txtParas[i].classList.remove("highlight");
+  }
+  this.classList.add("highlight");
+  selected = this.innerHTML;
+  console.log(selected);
+}
+
+function addToSpeedDial() {
+  // Add the current selected text to localStorage
+  localStorage.setItem("speedDial", selected);
+}
